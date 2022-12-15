bbcc38badb4281812904e39a0a947ee55fede3cb
joaocout89 <jarscoutinho@protonmail.com>
2022-12-15 15:28:23 +0000

    button click

---
+++ project/script.js
@@ -0,0 +1,6 @@
+ 
+
+
+document.getElementById("change-video").addEventListener("click", function() {
+    document.getElementById('ytVideo').src = "https://www.youtube.com/embed/bo2KQer1KNM";
+ });
