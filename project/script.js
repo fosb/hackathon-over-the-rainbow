5cebc970f61daba83c02bb10cd7c7c2d351fdde2
joaocout89 <jarscoutinho@protonmail.com>
2022-12-15 16:04:51 +0000

    randomizer

--- project/script.js
+++ project/script.js
@@ -1,6 +1,12 @@
- 
+const songlist = [
+    'https://www.youtube.com/embed/H53QBgHFJUg',
+    'https://www.youtube.com/embed/XW5nIlIfR5s',
+    'https://www.youtube.com/embed/V1bFr2SWP1I',
+    'https://www.youtube.com/embed/iwlFTYT2Fdw',
+]
 
+let random = Math.floor(Math.random()*songlist.length)
 
 document.getElementById("change-video").addEventListener("click", function() {
-    document.getElementById('ytVideo').src = "https://www.youtube.com/embed/bo2KQer1KNM";
+    document.getElementById('ytVideo').src = songlist[random];
  });
